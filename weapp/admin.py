1d687ddef5f7ae6aeed4fccec64fe06007878b4c
Administrator <shahzadamjad94@gmail.com>
2022-02-28 12:29:54 +0500

    update

---
+++ weapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import documents
+# from .models import folders
+# from .models import topics
+# Register your models here.
+
+admin.site.register(documents)
+
+# admin.site.register(folders)
+
+# admin.site.register(topics)
